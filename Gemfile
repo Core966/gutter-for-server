783366cabc876c3842f306acc049cabc4381c198
Tibor Szalay <core966@indamail.hu>
2014-08-17 01:30:45 +0200

    Made the necessary changes

--- Gemfile
+++ Gemfile
@@ -12,10 +12,11 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer',  platforms: :ruby
+gem 'therubyracer',  platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'jquery-ui-rails', '1.1.0'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -38,3 +39,4 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+gem 'gutter'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,10 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( gutter/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( gutter/bootstrap-responsive.min.css )
+Rails.application.config.assets.precompile += %w( gutter/font-awesome/css/font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( gutter/style.css )
+Rails.application.config.assets.precompile += %w( gutter/pages/dashboard.css )
+Rails.application.config.assets.precompile += %w( gutter/odometer.css )
+
